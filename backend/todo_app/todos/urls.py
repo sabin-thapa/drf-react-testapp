acf3a8bdb2930b4eb783094850688c3053cd2a53
Sabin Thapa <sabint017@gmail.com>
2023-03-08 22:32:56 +0545

    feat: CRUD backend

--- backend/todo_app/todos/urls.py
+++ backend/todo_app/todos/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.apiView, name='api-view'),
     path('todos/', views.todoList, name='todo-list'),
     path('todo-detail/<str:pk>/', views.todoDetail, name='todo-detail'),
+    path('todo-create/', views.todoCreate, name='todo-create'),
+    path('todo-update/<str:pk>', views.todoUpdate, name='todo-update'),
+    path('todo-delete/<str:pk>', views.todoDelete, name='todo-delete'),
 ]
