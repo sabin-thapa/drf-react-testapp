f87ecca44047c7e3096083d7a6120234d1a55de1
Sabin Thapa <sabint017@gmail.com>
2023-03-09 00:47:00 +0545

    chore: update settings

--- backend/todo_app/todo_app/settings.py
+++ backend/todo_app/todo_app/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "todos.apps.TodosConfig"
+    "todos.apps.TodosConfig",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -123,3 +125,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
+]
